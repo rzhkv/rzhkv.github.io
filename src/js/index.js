9f3a5cbb139f17c6161f3c2942a43416de3255c9
Denis <rdrozhkov@gmail.com>
2023-08-04 17:04:10 +0300

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
-let typeSplit = new SplitType(".preloader-text", {
+import "modern-normalize";
+
+var typeSplit = new SplitType(".preloader-text", {
   types: "lines, words, chars",
   tagName: "span",
 });
 
-var tl = gsap.timeline({ });
+var tl = gsap.timeline({});
 
 tl.from(".preloader-text .char", {
   y: "100%",
@@ -13,4 +15,11 @@ tl.from(".preloader-text .char", {
   stagger: 0.1,
 });
 
-tl.to(".preloader", { opacity: 0, display: 'none' }, "+=1");
+tl.to(".preloader", { opacity: 0, display: "none" }, "+=1");
+
+tl.from(".main .section, .main .header, .main .footer", {
+  opacity: 0,
+  duration: 1,
+  ease: "power1.out",
+  stagger: 0.1,
+});
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "",
-  root: "src",
-  server: {
-    host: true,
-    port: 3000,
-  },
+  server: {},
 });
